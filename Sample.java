151068c9b87141e69d49d8042777eb0d36090629
shwenzhang <shaowen.zsw@alibaba-inc.com>
2019-02-20 03:51:27 +0800

    commit sample files

---
+++ Sample.java
@@ -0,0 +1,12 @@
+
+public class Sample {
+    private static final char[] DATA = {
+      'A','m','b','e','r',
+      ' ','u','s','e','s', ' ',
+      'A','n','d','r','o','i','d'
+    };
+
+    public void test() {
+        System.out.print("I am a test sample!");
+    }
+}
